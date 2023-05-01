c964a6c896db14d98140cc3d4076cfb43c3b047d
Dmitrii Shagarov <shagarov07@gmail.com>
2023-05-01 19:57:32 +0300

    start second lng

---
+++ lngArr.js
@@ -0,0 +1,36 @@
+const langArr = {
+    "order__button":{
+        "ru":"",
+        "en":"order",
+    },
+    "about":{
+        "ru":"",
+        "en":"about",
+    },
+    "about__text":{
+        "ru":"",
+        "en":`Lorem ipsum dolor sit amet consectetur adipisicing elit
+        Tempora dolorum voluptatibus eius distinctio, voluptatum
+        ratione voluptate quos dicta at delectus non assumenda
+        doloremque quisquam voluptates totam obcaecati est 
+        aspernatur voluptatem praesentium possimus minima 
+        consectetur repudiandae.
+        Incidunt quidem cumque laboriosam quisquam?`,
+    },
+    "about":{
+        "ru":"",
+        "en":"about",
+    },
+    "about":{
+        "ru":"",
+        "en":"about",
+    },
+    "about":{
+        "ru":"",
+        "en":"about",
+    },
+    "about":{
+        "ru":"",
+        "en":"about",
+    },
+}
